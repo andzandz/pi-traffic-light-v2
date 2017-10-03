41e1f0153c02764bd1988cbf5536d417cedc3311
andzandz <andzandz@users.noreply.github.com>
2017-10-03 23:54:27 +0100

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -2,4 +2,4 @@
 # --host=0.0.0.0: Run on all interfaces (allow other devices to browse)
 # --port=80: The default port number that web browsers use (HTTP), so instead of typing 192.168.1.1:80 into
 # a browser you can just use 192.168.1.1
-sudo FLASK_APP=/home/pi/wifi_traffic_light/server.py flask run --host=0.0.0.0 --port=80
+sudo FLASK_APP=/home/pi/pi-traffic-light-v2/server.py flask run --host=0.0.0.0 --port=80
